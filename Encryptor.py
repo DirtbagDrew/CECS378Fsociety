0f3bbccbfc86b310a6672df4b49cd48542c77634
Mario Lopez <Mario.LopezHerdandez@student.csulb.edu>
2017-10-28 14:43:34 -0700

    Added ability to write to existing file

--- Encryptor.py
+++ Encryptor.py
@@ -4,6 +4,8 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
+from tkinter import Tk #used for GUI file picker
+from tkinter.filedialog import askopenfilename
 
 def Myencrypt(message, key):
 	if len(key) < 32:
@@ -12,10 +14,10 @@ def Myencrypt(message, key):
 	
 	padder = padding.PKCS7(128).padder() #Message must be padded to match block size
 	try:
-		message = message.encode()
-	except (AttributeError):
+		message = message.encode() #encode() is used to turn message into byte representation
+	except (AttributeError): #handles exception if mesage is already a byte 
 		pass
-	padded_message = padder.update(message) #encode() is used to turn message into byte representation
+	padded_message = padder.update(message) 
 	padded_message = padder.finalize()
 	
 	backend = default_backend()
@@ -35,25 +37,31 @@ print ("Cipher Text: ",C)
 
 #Used if you want to write the cipher text to a file
 def WritetoFile(cipher):
-	file = open('Ciphertext.txt', 'wb') 
+	while True:
+		selection = input("Select an Option: \n1. Write to existing File \n2. Create new File\n")
+		if selection == '1':
+			Tk().withdraw()
+			filename = askopenfilename()
+			break
+		elif selection == '2':
+			filename = input("Enter the name of the txt file you would like to create: ")
+			break
+		else:
+			print ("Incorrect Input. Please try again and type 1 or 2.")
+	file = open(filename, 'wb') 
 	file.write(cipher)
 	file.close
 	print ("File succesfully created")
 
-
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 Tk().withdraw()
 filename = askopenfilename()
-
-#message = open(filename, 'rb')
-#print(message.read())
 
 def MyfileEncrypt(filepath):
 	key = os.urandom(32)
 	message = open(filepath, 'rb') #opens the file in it's byte representation
 	C, IV = Myencrypt(message.read(), key) #reads file as a string of bytes
+	message.close
+	WritetoFile(C)
 	return C, IV, key
 C, IV, key = MyfileEncrypt(filename)
-WritetoFile(C)
 print("Program ran succesfully")
