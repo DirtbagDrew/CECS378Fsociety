d550208431d713344e03878fcdceb0641206eb7f
Mario Lopez <Mario.LopezHerdandez@student.csulb.edu>
2017-10-29 21:17:09 -0700

    Better user experience

--- Encryptor.py
+++ Encryptor.py
@@ -5,7 +5,8 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from tkinter import Tk #used for GUI file picker
-from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename #Dialog for opening/saving files
+from pathlib import Path #used to get file ext 
 
 def Myencrypt(message, key):
 	if len(key) < 32:
@@ -28,12 +29,13 @@ def Myencrypt(message, key):
 	return C, IV
 
 #Used for writing the cipher text to a file
-def WritetoFile(cipher):
-	input("Press [enter] to select new or existing save location of encyrpted file")
+def WritetoFile(cipher, ext):
 	Tk().withdraw()
-	filename = asksaveasfilename(title="Select Save Location")
+	if ext != None: #used to see if a file ext was passed in
+		filename = asksaveasfilename(title="Select Save Location", filetypes = [(ext, ext)] )
+	else:
+		filename = asksaveasfilename(title="Select Save Location", filetypes = [("All Files","*.*")])
 	file = open(filename, "wb")
-	#print(cipher)
 	file.write(cipher)
 	file.close
 	print ("File succesfully created")
@@ -41,36 +43,37 @@ def WritetoFile(cipher):
 def MyfileEncrypt(filepath):
 	key = os.urandom(32);
 	message = open(filepath, 'rb') #opens the file in its byte representation
-	#print(message)
 	C, IV = Myencrypt(message.read(), key) #reads file as a string of bytes
 	message.close
-	WritetoFile(C)
-	return C, IV, key
+	ext = Path(filepath).suffix # grabs extension of file
+	input("Press [enter] to select new or existing save location of encyrpted file")
+	WritetoFile(C,None)
+	return C, IV, key, ext
+	
+def Decryptor(IV, key, ext):
+	Tk().withdraw()
+	filename = askopenfilename(title = "Select File to Decrypt")
+	EncMessage = open(filename, "rb")
+
+	backend = default_backend()
+	cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)
+	decryptor = cipher.decryptor() #decrypts
+	m = decryptor.update(EncMessage.read()) + decryptor.finalize()
+
+	unpadder = padding.PKCS7(128).unpadder() #unpads
+	data = unpadder.update(m) 
+	data += unpadder.finalize()
+	input("Press [enter] to select save location of decrypted file")
+	WritetoFile(data, ext) #saves to file
 
 input("Press [enter] to select the file you would like to encyrpt")
 Tk().withdraw()
 filename = askopenfilename(title="Select File to Encrypt")
-C, IV, key = MyfileEncrypt(filename)
+C, IV, key, ext = MyfileEncrypt(filename)
+input("Press [enter] to select file to decrypt")
+Decryptor(IV, key, ext)
+
 print("Program ran succesfully")
 
-#######################################################################
-#Starting the Decryption process; Not currently working
-#######################################################################
-"""
-Tk().withdraw()
-filename = askopenfilename()
-EncMessage = open(filename, "rb")
 
-#Peliminal Decryptor; Needs formating
-backend = default_backend()
-cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)
-decryptor = cipher.decryptor()
-#print(EncMessage)
-m = decryptor.update(C) + decryptor.finalize()
-#print(m)
 
-unpadder = padding.PKCS7(128).unpadder()
-data = unpadder.update(m)
-data += unpadder.finalize()
-WritetoFile(data)
-"""
